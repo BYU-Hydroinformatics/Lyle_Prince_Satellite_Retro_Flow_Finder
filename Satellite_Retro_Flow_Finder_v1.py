4c6530ab12c781ec101414daffc87818fc4f1eba
ldp48 <prince414@gmail.com>
2024-02-15 15:55:42 -0700

    Rough draft of code

--- Satellite_Retro_Flow_Finder_v1.py
+++ Satellite_Retro_Flow_Finder_v1.py
@@ -2,8 +2,9 @@ import xarray as xr
 from typing import Tuple
 from geoglows import streamflow
 import ee
-import hydrafloods
+import hydrafloods as hf
 import datetime
+import pandas as pd
 
 def get_streamflow(lat: float, lon: float) -> Tuple[xr.DataArray, int]:
     """Function to get histroical streamflow data from the GeoGLOWS server
@@ -17,19 +18,19 @@ def get_streamflow(lat: float, lon: float) -> Tuple[xr.DataArray, int]:
         xr.DataArray: DataArray object of streamflow with datetime coordinates
     """
     # This gets the reach number from a point, will need to be rewritten for V2
-    reach = streamflow.latlon_to_reach(lat,lon)
+    reach = streamflow.latlon_to_reach(lat, lon)
     # This gets the data, will need to be rewritten for V2
     q = streamflow.historic_simulation(reach['reach_id'])
+    return q, reach['reach_id']
 
-    # rename column name to something not as verbose as 'streamflow_m^3/s'
-    q.columns = ["discharge"]
+def format_flow_dates(dates: pd.DataFrame) -> xr.DataArray:
 
     # rename index and drop the timezone value
-    q.index.name = "time"
-    q.index = q.index.tz_localize(None)
+    dates.index.name = "time"
+    dates.index = dates.index.tz_localize(None)
 
     # return the series as a xr.DataArray
-    return q.discharge.to_xarray(), reach['reach_id']
+    return dates.discharge.to_xarray()
 
 def get_image_dates(lat: float, lon: float):
     region = ee.Geometry.BBox(lat+.001, lon+.001, lat-.001, lon-.001)
@@ -39,7 +40,9 @@ def get_image_dates(lat: float, lon: float):
     dates = s1.dates
     dates_df = pd.DataArray(dates)
     dates_df = [datetime.datetime.utcfromtimestamp(i/1000).strftime('%Y-%m-%d') for i in dates_df]
-    
+    dates_df.index.name = "time"
+    dates_array = dates_df.to_xarray()
+    return dates_array
 
 def match_dates(original: xr.DataArray, matching: xr.DataArray) -> xr.DataArray:
     """Helper function to filter a DataArray from that match the data values of another.
@@ -55,3 +58,5 @@ def match_dates(original: xr.DataArray, matching: xr.DataArray) -> xr.DataArray:
 
     # return the DataArray with only rows that match dates
     return original.where(original.time.isin(matching.time), drop=True)
+
+def calculate_statistics
--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import Satellite_Retro_Flow_Finder_v1
+import geoglows
+
+#inputs
+lat = 0
+lon = 0
+return_period = 10
+
+# get streamflows from GeoGLOWS
+q_geo = Satellite_Retro_Flow_Finder_v1.get_streamflow(lat, lon)
+reach_id = q_geo[1]
+q_geo = q_geo[0]
+q_geo_xarray = Satellite_Retro_Flow_Finder_v1.format_flow_dates(q_geo)
+
+#get image dates
+dates_imgs = Satellite_Retro_Flow_Finder_v1.get_image_dates(lat, lon)
+
+#match dates
+dates_flows_xr = Satellite_Retro_Flow_Finder_v1.match_dates(q_geo_xarray, dates_imgs)
+
+#calculate return period
+return_period = geoglows.return_period(reach_id)
+
 
